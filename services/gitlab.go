6f2fe7ebe729d7d4a9710ea918e4e74049e6b587
sushil shinde <sushilshinde2007@gmail.com>
2020-02-28 20:39:06 +0000

    Gitlab integration

---
+++ services/gitlab.go
@@ -0,0 +1,50 @@
+package services
+
+import (
+	"github.com/xanzy/go-gitlab"
+	"goscrum/server/models"
+)
+
+type GitlabClient struct{}
+
+func (GitlabClient) Client() *gitlab.Client {
+	return gitlab.NewClient(nil, "<private-token>")
+}
+
+type GitlabService struct {
+	client *GitlabClient
+}
+
+func NewGitlabPService() *GitlabService {
+	return &GitlabService{client: &GitlabClient{}}
+}
+
+func (service GitlabService) Commits(req models.RequestCommits, id string) ([]*gitlab.Commit, error) {
+	commits, _, err := service.client.Client().Commits.ListCommits(id, &gitlab.ListCommitsOptions{Since: gitlab.Time(req.Since)})
+	return commits, err
+}
+
+func (service GitlabService) Issues(req models.RequestIssues) ([]*gitlab.Issue, error) {
+	issues, _, err := service.client.Client().Issues.ListIssues(&gitlab.ListIssuesOptions{
+		ListOptions:  gitlab.ListOptions{},
+		State:        gitlab.String(req.State),
+		AuthorID:     gitlab.Int(req.AuthorID),
+		AssigneeID:   gitlab.Int(req.AssigneeID),
+		Search:       gitlab.String(req.Search),
+		UpdatedAfter: gitlab.Time(req.UpdatedAfter),
+	})
+	return issues, err
+}
+
+func (service GitlabService) Users(search string) ([]*gitlab.User, error) {
+	users, _, err := service.client.Client().Users.ListUsers(&gitlab.ListUsersOptions{Search: gitlab.String(search)})
+	return users, err
+}
+
+func (service GitlabService) Projects(search string) ([]*gitlab.Project, error) {
+	projects, _, err := service.client.Client().Projects.ListProjects(&gitlab.ListProjectsOptions{
+		Search:     gitlab.String(search),
+		Visibility: gitlab.Visibility(gitlab.PrivateVisibility),
+	})
+	return projects, err
+}
